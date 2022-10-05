ce71c8e054c64416f709d45cde4f31233da625c4
RahulVijay17 <frederickraghul@gmail.com>
2022-10-05 15:10:29 +0530

    stack

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack=new Stack(2);
+
+        myStack.getTop();
+        myStack.getHeight();
+        myStack.printStack();
+    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        Stack myStack= new Stack(2);
+        myStack.push(3);
+
+        myStack.printStack();
+    }
+}
